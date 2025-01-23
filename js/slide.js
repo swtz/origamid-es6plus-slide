843cb81387ac94f4206e4c48df27446920b974e7
Leonardo Lüders <luderser@hotmail.com>
2025-01-23 09:37:14 -0300

    class 1202 finished | created 'onEnd' method | principal functionality of slide done.

--- js/slide.js
+++ js/slide.js
@@ -6,14 +6,25 @@ export default class Slide {
 
   onStart(event) {
     event.preventDefault();
+    this.wrapper.addEventListener('mousemove', this.onMove);
+  }
+
+  onMove(event) {
+  }
+
+  onEnd(event) {
+    this.wrapper.removeEventListener('mousemove', this.onMove);
   }
 
   addSlideEvents() {
     this.wrapper.addEventListener('mousedown', this.onStart);
+    this.wrapper.addEventListener('mouseup', this.onEnd);
   }
 
   bindEvents() {
-    this.onStart = this.onStart(this);
+    this.onStart = this.onStart.bind(this);
+    this.onMove = this.onMove.bind(this);
+    this.onEnd = this.onEnd.bind(this);
   }
 
   init() {
@@ -21,5 +32,4 @@ export default class Slide {
     this.addSlideEvents();
     return this;
   }
-
 }
