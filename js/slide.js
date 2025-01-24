2749fef0067551109a1f154ce7a57f06fa80ba58
Leonardo Lüders <luderser@hotmail.com>
2025-01-24 10:55:51 -0300

    finished class 1206 (Slide Next e Prev) | created 'transition(arg: Boolean)' method to set value of style property 'transition'. It's used into 'init()' method and after user firing 'mouseup' or 'touchend' events. Into 'onStart' method, 'transition' method is called with arg value as 'false'.

--- js/slide.js
+++ js/slide.js
@@ -5,6 +5,10 @@ export default class Slide {
     this.dist = { finalPosition: 0, startX: 0, movement: 0 }
   }
 
+  transition(active) {
+    this.slide.style.transition = active ? 'transform .3s' : '';
+  }
+
   moveSlide(distX) {
     this.dist.movePosition = distX;
     this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
@@ -26,6 +30,7 @@ export default class Slide {
       moveType = 'touchmove';
     }
     this.wrapper.addEventListener(moveType, this.onMove);
+    this.transition(false);
   }
 
   onMove(event) {
@@ -38,6 +43,7 @@ export default class Slide {
     const moveType = (event.type === 'mouseup') ? 'mousemove' : 'touchmove';
     this.wrapper.removeEventListener(moveType, this.onMove);
     this.dist.finalPosition = this.dist.movePosition;
+    this.transition(true);
     this.changeSlideOnEnd();
   }
 
@@ -102,6 +108,7 @@ export default class Slide {
 
   init() {
     this.bindEvents();
+    this.transition(true);
     this.addSlideEvents();
     this.slidesConfig();
     return this;
